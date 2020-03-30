f413b9e2f8f9e562508a1a71d679294a46d5348a
Jordan <jordanjosephs072@gmail.com>
2020-03-29 22:40:13 -0400

    New Mod!

---
+++ src/main/java/com/thejoker/testingmod/blocks/TestBlock.java
@@ -0,0 +1,13 @@
+package com.thejoker.testingmod.blocks;
+
+import sun.jvm.hotspot.debugger.Address;
+import sun.jvm.hotspot.opto.Block;
+
+public class TestBlock extends Block
+{
+
+    public TestBlock(Address addr) {
+        super(addr);
+
+    }
+}
